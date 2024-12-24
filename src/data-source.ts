4790ad5f7369f73b2c8a6fac3d325e725d9ef277
Kenny <93316254+Kosmasu@users.noreply.github.com>
2024-12-24 16:31:58 +0800

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,7 +12,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT) || 3306,
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'rokobot',
   entities: [Tweet, Prompt, StoryPrompt, Chapter],
   migrations: ['src/migrations/*.ts'],
